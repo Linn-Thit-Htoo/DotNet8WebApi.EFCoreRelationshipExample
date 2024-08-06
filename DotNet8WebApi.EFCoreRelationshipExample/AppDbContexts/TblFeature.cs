527d6553369d1c16586b14117c4f2a24b8265c81
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-06 16:17:06 +0630

    Format Code

--- DotNet8WebApi.EFCoreRelationshipExample/AppDbContexts/TblFeature.cs
+++ DotNet8WebApi.EFCoreRelationshipExample/AppDbContexts/TblFeature.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace DotNet8WebApi.EFCoreRelationshipExample.AppDbContexts;
+﻿namespace DotNet8WebApi.EFCoreRelationshipExample.AppDbContexts;
 
 public partial class TblFeature
 {
@@ -9,5 +6,6 @@ public partial class TblFeature
 
     public string FeatureName { get; set; } = null!;
 
-    public virtual ICollection<TblPropertyFeature> TblPropertyFeatures { get; set; } = new List<TblPropertyFeature>();
+    public virtual ICollection<TblPropertyFeature> TblPropertyFeatures { get; set; } =
+        new List<TblPropertyFeature>();
 }
