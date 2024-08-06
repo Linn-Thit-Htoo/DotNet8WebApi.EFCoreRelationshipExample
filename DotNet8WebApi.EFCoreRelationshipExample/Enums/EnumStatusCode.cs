52fe7ac7b69bc62dceae82401e3b0f1f39574533
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-06 16:18:22 +0630

    Format Code

--- DotNet8WebApi.EFCoreRelationshipExample/Enums/EnumStatusCode.cs
+++ DotNet8WebApi.EFCoreRelationshipExample/Enums/EnumStatusCode.cs
@@ -1,16 +1,15 @@
-﻿namespace DotNet8WebApi.EFCoreRelationshipExample.Enums
+﻿namespace DotNet8WebApi.EFCoreRelationshipExample.Enums;
+
+public enum EnumStatusCode
 {
-    public enum EnumStatusCode
-    {
-        Success = 200,
-        Created = 201,
-        Accepted = 202,
-        BadRequest = 400,
-        UnAuthorized = 401,
-        NotFound = 404,
-        MethodNotAllowed = 405,
-        Conflict = 409,
-        Locked = 423,
-        InternalServerError = 500
-    }
+    Success = 200,
+    Created = 201,
+    Accepted = 202,
+    BadRequest = 400,
+    UnAuthorized = 401,
+    NotFound = 404,
+    MethodNotAllowed = 405,
+    Conflict = 409,
+    Locked = 423,
+    InternalServerError = 500
 }
