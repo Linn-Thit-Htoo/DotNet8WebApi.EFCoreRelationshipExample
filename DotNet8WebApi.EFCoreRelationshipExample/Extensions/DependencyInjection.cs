0894861b908a43f08714eeaa8404daef511600bd
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-06 16:18:33 +0630

    Format Code

--- DotNet8WebApi.EFCoreRelationshipExample/Extensions/DependencyInjection.cs
+++ DotNet8WebApi.EFCoreRelationshipExample/Extensions/DependencyInjection.cs
@@ -1,30 +1,38 @@
-﻿namespace DotNet8WebApi.EFCoreRelationshipExample.Extensions
-{
-    public static class DependencyInjection
-    {
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            services.AddDbContextService(builder)
-                .AddRepositoryService();
-            return services;
-        }
+﻿namespace DotNet8WebApi.EFCoreRelationshipExample.Extensions;
 
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Services.AddDbContext<EfcoreTableJoinContext>(opt =>
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDependencyInjection(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
+    {
+        services.AddDbContextService(builder).AddRepositoryService();
+        return services;
+    }
+
+    private static IServiceCollection AddDbContextService(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
+    {
+        builder.Services.AddDbContext<EfcoreTableJoinContext>(
+            opt =>
             {
                 opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
                 opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+            },
+            ServiceLifetime.Transient,
+            ServiceLifetime.Transient
+        );
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddRepositoryService(this IServiceCollection services)
-        {
-            services.AddScoped<IFeatureRepository, FeatureRepository>();
-            services.AddScoped<IPropertyRepository, PropertyRepository>();
-            return services;
-        }
+    private static IServiceCollection AddRepositoryService(this IServiceCollection services)
+    {
+        services.AddScoped<IFeatureRepository, FeatureRepository>();
+        services.AddScoped<IPropertyRepository, PropertyRepository>();
+        return services;
     }
 }
