64c8e7f5dd8c3867e3201e5f37cb10b5f0c702dd
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-06 16:21:21 +0630

    Format Code

--- DotNet8WebApi.EFCoreRelationshipExample/Repositories/Feature/IFeatureRepository.cs
+++ DotNet8WebApi.EFCoreRelationshipExample/Repositories/Feature/IFeatureRepository.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNet8WebApi.EFCoreRelationshipExample.Repositories.Feature
+﻿namespace DotNet8WebApi.EFCoreRelationshipExample.Repositories.Feature;
+
+public interface IFeatureRepository
 {
-    public interface IFeatureRepository
-    {
-        Task<Result<FeatureListResponseModel>> GetFeatureList();
-        Task<Result<FeatureResponseModel>> CreateFeature(FeatureRequestModel requestModel);
-        Task<Result<FeatureResponseModel>> UpdateFeature(FeatureRequestModel requestModel, string id);
-        Task<Result<FeatureResponseModel>> DeleteFeature(string id);
-    }
+    Task<Result<FeatureListResponseModel>> GetFeatureList();
+    Task<Result<FeatureResponseModel>> CreateFeature(FeatureRequestModel requestModel);
+    Task<Result<FeatureResponseModel>> UpdateFeature(FeatureRequestModel requestModel, string id);
+    Task<Result<FeatureResponseModel>> DeleteFeature(string id);
 }
