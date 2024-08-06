2df3c8627291acb1b2fd9b8b0d94b5aea15a38bb
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-06 14:29:21 +0630

    -

--- DotNet8WebApi.EFCoreRelationshipExample/Program.cs
+++ DotNet8WebApi.EFCoreRelationshipExample/Program.cs
@@ -1,8 +1,12 @@
+using DotNet8WebApi.EFCoreRelationshipExample.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDependencyInjection(builder);
 
 var app = builder.Build();
 
