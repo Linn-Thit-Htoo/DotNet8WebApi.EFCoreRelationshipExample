ef57e448a849576a7e16c59a986f825888b03fcd
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-06 14:31:28 +0630

    [Added] Base Controller

--- DotNet8WebApi.EFCoreRelationshipExample/Controllers/FeatureController.cs
+++ DotNet8WebApi.EFCoreRelationshipExample/Controllers/FeatureController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DotNet8WebApi.EFCoreRelationshipExample.Repositories.Feature;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet8WebApi.EFCoreRelationshipExample.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class FeatureController : ControllerBase
+    public class FeatureController : BaseController
     {
+        private readonly IFeatureRepository _featureRepository;
+
+        public FeatureController(IFeatureRepository featureRepository)
+        {
+            _featureRepository = featureRepository;
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> GetFeature()
+        //{
+
+        //}
     }
 }
